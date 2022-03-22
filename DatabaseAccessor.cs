b433690e10c0a0c3da33f8616f7acfcd9a7dcf61
Oliver Madsen <oama38711@edu.ucl.dk>
2022-03-22 10:49:16 +0100

    dum dal

---
+++ DatabaseAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_transaktion_Forms
+{
+    class DatabaseAccessor
+    {
+        public static void ConnectToDatabase()
+        {
+            string strconn = @"Data Source=" + Environment.MachineName + ";Initial Catalog=Fly;Integrated Security=True;TrustServerCertificate=True";
+        }
+    }
+}
